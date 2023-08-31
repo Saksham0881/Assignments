20859b9255d834aa949932fb1184073872fbf11c
Saksham0881 <143702047+Saksham0881@users.noreply.github.com>
2023-08-31 15:20:32 +0530

    divisibility

--- c programing/output/output/output/if_else/evenodd.c
+++ c programing/output/output/output/if_else/evenodd.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+    if(n%3==0){
+        printf("%d is divisible by 3",n);
+}
+    else if (n%3!=0){
+        printf("%d is not divisible by 3",n);
+    }
+    return 0;
+}
